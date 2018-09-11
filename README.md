c89a47341fe4f5836c0aeb6828239d64b2033fb2
Reindert Korver <reindertkorver@gmail.com>
2018-09-11 17:13:26 +0200

    Initial commit
    11-9-18

---
+++ README.md
@@ -0,0 +1,8 @@
+# hoornbeeck_rooster_info_app
+
+This is an app that displays a schedule like roosterinfo.nl
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new StartPage());
+
+class StartPage extends StatefulWidget {
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
